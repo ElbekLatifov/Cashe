cc5e5cdbaca1fa07f599793d3d65a16575d96ddc
ElbekLatifov <latifovelbek02@gmail.com>
2023-06-10 22:51:02 +0500

    Добавьте файлы проекта.

---
+++ Controllers/CasheController.cs
@@ -0,0 +1,46 @@
+﻿using CasheApplication.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasheApplication.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CasheController : ControllerBase
+{
+    private readonly CasheService _service;
+
+    public CasheController(CasheService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(int a, int b)
+    {
+        int result = a + b;
+        await Task.Delay(5000);
+        return Ok(result);
+    }
+
+    [HttpPost("cashe")]
+    public async Task<IActionResult> Add2(int a, int b)
+    {
+        var key = $"{a},{b}";
+
+        int result = a + b;
+
+        if(_service.Get(key, out var value))
+        {
+            return Ok(value);
+        }
+        
+         _service.Set(key, result);    
+
+        await Task.Delay(5000);
+
+        return Ok(result);
+
+    }
+
+}
---
+++ Services/CasheService.cs
@@ -0,0 +1,29 @@
+﻿namespace CasheApplication.Services;
+public class CasheService
+{
+    public Dictionary<string, object> CasheDictionary { get; set; } = new Dictionary<string, object>();
+
+    public void Set(string key, object value)
+    {
+        CasheDictionary.Add(key, value); 
+    }
+
+    public bool Get(string key, out object? value)
+    {
+        //if (CasheDictionary.ContainsKey(key))
+        //{
+        //    value = CasheDictionary[key];
+        //    return true;
+        //}
+        //else
+        //{
+        //    value = null;
+        //    return false;
+        //}
+        return CasheDictionary.TryGetValue(key, out value);
+    }
+
+
+}
+
+
